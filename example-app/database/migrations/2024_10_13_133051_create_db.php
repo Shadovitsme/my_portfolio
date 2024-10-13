5f1925269978416157935013031c8625cd05c6a3
ukki <iu.cashirskaia@yandex.ru>
2024-10-13 21:15:18 +0300

    Полностью работающая миграция

--- example-app/database/migrations/2024_10_13_133051_create_db.php
+++ example-app/database/migrations/2024_10_13_133051_create_db.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,33 +14,58 @@ return new class extends Migration
     {
         Schema::create('my_stitches', function (Blueprint $table) {
             $table->id()->primary()->autoIncrement();
+            $table->binary('stitches');
             $table->timestamps();
         });
-        Schema::create('my_animals', function (Blueprint $table) {
+        Schema::create('my_pets', function (Blueprint $table) {
             $table->id()->primary()->autoIncrement();
+            $table->binary('pets');
             $table->timestamps();
         });
         Schema::create('my_plants', function (Blueprint $table) {
             $table->id()->primary()->autoIncrement();
+            $table->binary('plants');
             $table->timestamps();
         });
         Schema::create('skills_level', function (Blueprint $table) {
-            $table->id()->primary()->autoIncrement();
-            $table->timestamps();
-        });
-        Schema::create('skills', function (Blueprint $table) {
-            $table->id()->primary()->autoIncrement();
+            $table->id()->primary()
+                ->autoIncrement();
+            $table->integer('level');
             $table->timestamps();
         });
         Schema::create('projects', function (Blueprint $table) {
             $table->id()->primary()->autoIncrement();
+            $table->binary('icons');
+            $table->string('name');
+            $table->string('link_to_demo');
+            $table->string('link_to_code');
+            $table->boolean('commercial');
+            $table->timestamps();
+        });
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id()->primary()->autoIncrement();
+            $table->string('name');
+            $table->unsignedBigInteger('level_id');
+            $table->foreign('level_id')->references('id')->on('skills_level');
+            $table->foreignId('where_used_id')->nullable()->references('id')->on('projects');
             $table->timestamps();
         });
         Schema::create('my_drafts', function (Blueprint $table) {
             $table->id()->primary()->autoIncrement();
-            $table->string('drafts');
+            $table->binary('drafts');
             $table->timestamps();
         });
+        DB::table('skills_level')->insert([
+            ['level' => '1'],
+            ['level' => '2'],
+            ['level' => '3'],
+            ['level' => '4'],
+            ['level' => '5'],
+        ]);
+        DB::table('skills')->insert([
+            ['name' => 'PHP', 'level_id' => '3'],
+            ['name' => 'Docker', 'level_id' => '3'],
+        ]);
     }
 
     /**
@@ -47,6 +73,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('my_stitches,my_animals,my_plants,siells_level,skills,projects');
+        Schema::dropIfExists('my_stitches');
+        Schema::dropIfExists('my_pets');
+        Schema::dropIfExists('my_plants');
+        Schema::dropIfExists('skills_level');
+        Schema::dropIfExists('skills');
+        Schema::dropIfExists('projects');
     }
 };
