6acc25e113393550cb2def745896b5604eb07439
ukki <iu.cashirskaia@yandex.ru>
2024-10-16 00:38:54 +0300

    По сути готовая верстка раздела навыков - Добавлена таблица в раздел навыков - добавлены блоки для корректных отступов между таблицей и блоком аякса

--- example-app/resources/views/skills.blade.php
+++ example-app/resources/views/skills.blade.php
@@ -13,20 +13,7 @@
     <x-header>$head</x-header>
     <div class="padding"></div>
     <x-contacts>$contacts</x-contacts>
-    <table>
-        <!-- TODO сделать заполнение таблицы из бд -->
-        <!-- TODO сделать оформление по цсс -->
-        <tr>
-            <th>Навыки</th>
-            <th>Уровень</th>
-            <th>Где применялось</th>
-        </tr> <!--ряд с ячейками заголовков-->
-        <tr>
-            <td>данные</td>
-            <td>данные</td>
-        </tr> <!--ряд с ячейками тела таблицы-->
-    </table>
-
+    <!-- TODO добавить кнопку найти -->
     <div class="ajaxMenuBeckground">
         <div class="ajaxBlocks">
             <span>Собери свой стек!</span>
@@ -46,8 +33,53 @@
             <button value='0' class="button-not-selected" id="four">4</button>
             <button value='0' class="button-not-selected" id="five">5</button>
         </div>
+        <div class="map_legend">
+            <table class="map_legend_table">
+                <tr>
+                    <td id="one">1</td>
+                    <td>Базовые навыки</td>
+                </tr>
+                <tr>
+                    <td id="two">2</td>
+                    <td>Ниже среднего</td>
+                </tr>
+                <tr>
+                    <td id="tre">3</td>
+                    <td>Средний уровень</td>
+                </tr>
+                <tr>
+                    <td id="four">4</td>
+                    <td>Выше среднего</td>
+                </tr>
+                <tr>
+                    <td id="five">5</td>
+                    <td>Владею <br /> на высоком уровне</td>
+                </tr>
+
+            </table>
+        </div>
 
     </div>
+    <div class="big-table">
+        <table class="skillTable">
+            <!-- TODO сделать заполнение таблицы из бд -->
+            <!-- TODO сделать оформление по цсс -->
+            <tr>
+                <td class="first_column">Навыки</td>
+                <td class="second_column">Уровень</td>
+                <td class="third_column">Где применялось</td>
+            </tr> <!--ряд с ячейками заголовков-->
+            <tr>
+                <td>данные</td>
+                <td>даdjfalkdsjklsdjlkjsldjaslkdjlksdjlkjslkdjfнные</td>
+                <!-- TODO сделать так чтобы длинный текст автоматом переносился а не уплывал -->
+                <td>данdsafljklfjsdklfjsdlfjdslfjksdjflksdjldsjfljsdlfjsljsdlfjsalkные</td>
+            </tr> <!--ряд с ячейками тела таблицы-->
+        </table>
+
+    </div>
+
+
 </body>
 
 </html>
