63ad039d46fb689eddd2db35fae5b97ba0f07184
ukki <iu.cashirskaia@yandex.ru>
2024-10-16 11:02:49 +0300

    Готовая верстка -доделана верстка на вкладке портфолио

--- example-app/resources/views/portfolio.blade.php
+++ example-app/resources/views/portfolio.blade.php
@@ -12,17 +12,32 @@
     <x-header>$head</x-header>
     <div class="padding"></div>
     <x-contacts>$contacts</x-contacts>
-    <div class="textcols">
-        <!-- TODO Заполнить это из бд -->
-        <div class="textcols-item">
-            <img class="main-page-image" src="images/plaseholder.webp">
-        </div>
-        <div class="textcols-item">
-            <a href="https://github.com/Shadovitsme">демонстрация</a>
-        </div>
-        <div class="textcols-item">
-            <a>код</a>
-        </div>
+    <div class="big-table-portfolio">
+        <table class="PortfolioTable">
+            <!-- TODO сделать заполнение таблицы из бд -->
+            <!-- TODO сделать оформление по цсс -->
+            <tr>
+                <th class="first_column_port"></th>
+                <th class="second_column_port">Демонстрация</th>
+                <th class="third_column_port">Код</th>
+                <th class="four_column_port">Описание</th>
+            </tr> <!--ряд с ячейками заголовков-->
+            <tr>
+                <td><img class="portfolioIcon" src="images/plaseholder.webp"></td>
+                <td>даdjfaddddddddddddddddddddddddddddddddddddddddddddddddddddlkdsjklsdjlkjsldjaslkdjlksdjlkjslkdjfнные</td>
+                <!-- TODO сделать так чтобы длинный текст автоматом переносился а не уплывал -->
+                <td>данdsafljklfjsdklfjsdlfjdslfjksdjflksdjldsjfljsdlfjsljsdlfjsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddalkные</td>
+                <td>данdsafljklfjsdklfjsdlfjdslfjksdjflksdjldsjfljsdlfjsljsdlfjsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddalkные</td>
+            </tr> <!--ряд с ячейками тела таблицы-->
+            <tr>
+                <td><img class="portfolioIcon" src="images/plaseholder.webp"></td>
+                <td>даdjfaddddddddddddddddddddddddddddddddddddddddddddddddddddlkdsjklsdjlkjsldjaslkdjlksdjlkjslkdjfнные</td>
+                <!-- TODO сделать так чтобы длинный текст автоматом переносился а не уплывал -->
+                <td>данdsafljklfjsdklfjsdlfjdslfjksdjflksdjldsjfljsdlfjsljsdlfjsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddalkные</td>
+                <td>данdsafljklfjsdklfjsdlfjdslfjksdjflksdjldsjfljsdlfjsljsdlfjsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddalkные</td>
+            </tr> <!--ряд с ячейками тела таблицы-->
+        </table>
+
     </div>
 </body>
 
